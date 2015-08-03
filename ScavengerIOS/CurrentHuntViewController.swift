db9adc0d1b3e5680f372a48e0ccffa7328888762
kkane106 <kkane106@gmail.com>
2015-08-03 14:11:33 -0400

    Add temporary button to Current Hunt/mapview Add link between current hunt vc and CurrentHuntViewController via IBOutlet

---
+++ ScavengerIOS/CurrentHuntViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentHuntViewController.swift
+//  ScavengerIOS
+//
+//  Created by Kristopher Kane on 8/3/15.
+//  Copyright (c) 2015 Kris Kane. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CurrentHuntViewController: UIViewController {
+    @IBOutlet weak var huntMap: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
