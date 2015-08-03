fa74bfaf8aa04db133e42cb090b7232d190d2be1
kkane106 <kkane106@gmail.com>
2015-08-03 14:15:36 -0400

    Add ListHuntsViewController

---
+++ ScavengerIOS/ListHuntsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListHuntsViewController.swift
+//  ScavengerIOS
+//
+//  Created by Kristopher Kane on 8/3/15.
+//  Copyright (c) 2015 Kris Kane. All rights reserved.
+//
+
+import UIKit
+
+class ListHuntsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
