9ab8b1675519545b1d992501f792c65b2e245ba0
kkane106 <kkane106@gmail.com>
2015-08-03 14:46:49 -0400

    Add UITableView properties to ListHunts, add methods to make it conform to protocol

--- ScavengerIOS/ListHuntsViewController.swift
+++ ScavengerIOS/ListHuntsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ListHuntsViewController: UIViewController {
+class ListHuntsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var scavengerHuntsTableView: UITableView!
     
@@ -17,5 +17,16 @@ class ListHuntsViewController: UIViewController {
 
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
 }
