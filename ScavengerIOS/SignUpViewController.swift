cd3aa3c3aad0caf95063c13121cb042b5f4da3ff
kkane106 <kkane106@gmail.com>
2015-08-09 17:56:45 -0400

    Add functioning User SignUp

--- ScavengerIOS/SignUpViewController.swift
+++ ScavengerIOS/SignUpViewController.swift
@@ -1,33 +1,42 @@
 //
-//  LoginViewController.swift
+//  SignUpViewController.swift
 //  ScavengerIOS
 //
 //  Created by Kristopher Kane on 8/9/15.
 //  Copyright (c) 2015 Kris Kane. All rights reserved.
 //
 
+
 import UIKit
 import Parse
 
-class LoginViewController: UIViewController {
+class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func doVerifyLogin(sender: UIButton) {
-        if emailTextField.text != "" && passwordTextField.text != "" {
-            
+        if emailTextField.text != "" && passwordTextField.text != "" && usernameTextField.text != ""{
+            userSignUp()
         } else {
             self.messageLabel.text = "Please enter a valid email and password"
         }
     }
     
-    func 
+    func userSignUp() {
+        var user = PFUser()
+        user.username = usernameTextField.text
+        user.password = passwordTextField.text
+        user.email = emailTextField.text
+
+        user.signUpInBackground()
+    }
     
 }
