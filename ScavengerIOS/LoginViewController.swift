662bc4d27a8b43f0c1711d1620c8456d04b271a8
kkane106 <kkane106@gmail.com>
2015-08-09 19:44:39 -0400

    Add userLogin(), store username in NSUserDefaults, store password in Keychain

--- ScavengerIOS/LoginViewController.swift
+++ ScavengerIOS/LoginViewController.swift
@@ -15,19 +15,61 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
     
+    @IBOutlet weak var loginButton: UIButton!
+    
+    let MyKeyChainWrapper = KeychainWrapper()
+    let createLoginButtonTag = 0
+    let loginButtonTag = 1
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     @IBAction func doVerifyLogin(sender: UIButton) {
-        view.endEditing(true)
-        if passwordTextField.text != "" && usernameTextField.text != ""{
-            userLogin()
-        } else {
-            self.messageLabel.text = "Please enter a valid email and password"
+        if (usernameTextField.text == "" || passwordTextField.text == "") {
+            var alert = UIAlertView()
+            alert.title = "You must enter both a username and password"
+            alert.addButtonWithTitle("Whoops!")
+            alert.show()
+            return
         }
-
+        
+        usernameTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+        userLogin()
+//
+//        if sender.tag == createLoginButtonTag {
+//            let hasLoginKey = defaults.boolForKey("hasLoginKey")
+//            if hasLoginKey == false {
+//                defaults.setValue(self.usernameTextField.text, forKey: "username")
+//            }
+//            
+//            MyKeyChainWrapper.mySetObject(passwordTextField.text, forKey: kSecValueData)
+//            MyKeyChainWrapper.writeToKeychain()
+//            defaults.setBool(true, forKey: "hasLoginKey")
+//            defaults.synchronize()
+//            loginButton.tag = loginButtonTag
+//            
+//            performSegueWithIdentifier("loginViewSegue", sender: self)
+//        } else if sender.tag == loginButtonTag {
+//            if checkLogin(usernameTextField.text, password: passwordTextField.text) {
+//                performSegueWithIdentifier("loginViewSegue", sender: self)
+//            }
+//        } else {
+//            var alert = UIAlertView()
+//            alert.title = "Login Problem"
+//            alert.message = "Wrong username or password"
+//            alert.addButtonWithTitle("Rats")
+//            alert.show()
+//        }
     }
     
     func userLogin() {
@@ -40,15 +82,66 @@ class LoginViewController: UIViewController {
                 }
             } else {
                 if let user = user {
+                    // If user record is returned
                     self.messageLabel.text = "Record found"
+                    
+                    // Look in UserDefaults to see if there is a loginKey
+                    let hasLoginKey = self.defaults.boolForKey("hasLoginKey")
+                    // If not, set the value of "username" to the current username in UserDefaults
+                    if hasLoginKey == false {
+                        self.defaults.setValue(self.usernameTextField.text, forKey: "username")
+                    }
+                    
+                    // Save the user password to Keychain
+                    self.MyKeyChainWrapper.mySetObject(self.passwordTextField.text, forKey: kSecValueData)
+                    self.MyKeyChainWrapper.writeToKeychain()
+                    // Set "hasLoginKey" to true
+                    self.defaults.setBool(true, forKey: "hasLoginKey")
+                    self.defaults.synchronize()
+                    
+                    self.performSegueWithIdentifier("loginViewSegue", sender: self)
+
                 }
+                
             }
         }
     }
-    @IBAction func doPresentSignUp(sender: UIButton) {
+    
+    // Ensure that the username matches what is stored in UserDefault and the password matches Keychain
+    func checkLogin(username: String, password:String) -> Bool {
+        let password = MyKeyChainWrapper.myObjectForKey("v_Data") as? String
+        let username = defaults.valueForKey("username") as? String
+        
+        if let password = password {
+            println("found a password in keychain")
+            if let username = username {
+                println("found a username in user defaults")
+                PFUser.logInWithUsernameInBackground(username, password: password) {
+                    (user: PFUser?, error: NSError?) -> Void in
+                    if error != nil {
+                        if let error = error {
+                            println("User not found")
+                            println(error)
+                            
+                        } else {
+                            if let user = user {
+                                println("parse returned a user")
+                            }
+                        }
+                    }
+                }
+                println("returned true")
+                return true
+            } else {
+                println("returned false")
+                return false
+            }
+        }
+            
+        return false
     }
     
-    @IBAction func doPresentForgotPassword(sender: UIButton) {
+    @IBAction func doPresentSignUp(sender: UIButton) {
     }
 
 }
