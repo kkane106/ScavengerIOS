9a94182fa6bd099ac6ea8f7c5d18e6e4a3b1514f
kkane106 <kkane106@gmail.com>
2015-08-19 15:33:42 -0400

    Add proximity to start location of scavenger hunts listed

--- ScavengerIOS/ListHuntsViewController.swift
+++ ScavengerIOS/ListHuntsViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 import MapKit
 import CoreLocation
 import Parse
+import Darwin // Math
+import Foundation
 
 class ListHuntsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
     @IBOutlet weak var scavengerHuntsTableView: UITableView!
+    
+    var currentGeoPoint : PFGeoPoint?
 
     let locManager = CLLocationManager()
     let cellID = "ScavengerHuntCell"
     var scavengerHuntToPass : PFObject?
     var searchDistance : Float = 10.0
+    
+    let π = M_PI
     
     var scavengerHunts = [PFObject]()
     
@@ -81,6 +87,27 @@ class ListHuntsViewController: UIViewController, UITableViewDelegate, UITableVie
 //        }
 //    }
     
+    // Haversine Formula for calculating proximity for display
+    func toRadians(degrees: Double) -> Double {
+        let radians = ( degrees * π) / 180
+        return radians
+    }
+    
+    func haversineFormula(lat1: Double, lat2: Double, lon1: Double, lon2: Double) -> Double  {
+        let R = 6371000.0 // Meters
+        let lat1R = toRadians(lat1)
+        let lat2R = toRadians(lat2)
+        let latDiff = toRadians(lat2 - lat1)
+        let lonDiff = toRadians(lon2 - lon1)
+        
+        var a = sin(latDiff/2) * sin(latDiff/2) + cos(lat1R) * cos(lat2R) * sin(lonDiff/2) * sin(lonDiff/2)
+        var c = 2 * atan2(sqrt(a), sqrt(1-a))
+        var d = R * c
+        
+        return d // In Meters
+    }
+    // end haversine
+    
     
     func updateScavengerHunts(radius : Float, completion : (response : [PFObject]) -> ()) {
         PFGeoPoint.geoPointForCurrentLocationInBackground { (geoPoint : PFGeoPoint?, error: NSError?) -> Void in
@@ -88,6 +115,7 @@ class ListHuntsViewController: UIViewController, UITableViewDelegate, UITableVie
                 println("no location found")
             } else {
                 if let geoPoint = geoPoint {
+                    self.currentGeoPoint = geoPoint
                     let userGeoPoint = geoPoint
                     var query = PFQuery(className: "ScavengerHunt")
                     query.whereKey("startingPoint", nearGeoPoint: userGeoPoint, withinMiles: 10.0)
@@ -113,7 +141,22 @@ class ListHuntsViewController: UIViewController, UITableViewDelegate, UITableVie
         let cell = tableView.dequeueReusableCellWithIdentifier(cellID) as! ScavengerHuntTableViewCell
         let currentHunt = scavengerHunts[indexPath.row]
         cell.scavengerHuntName?.text = currentHunt["name"] as! String
-        cell.scavengerHuntProximity?.text = currentHunt["description"] as! String
+        if let currentGeoPoint = currentGeoPoint {
+            if let startPoint = currentHunt["startingPoint"] as? PFGeoPoint {
+                println("START POINT! : \(startPoint)")
+                println("CURRENT GEOPOINT : \(currentGeoPoint)")
+                let proximity = haversineFormula(
+                    currentGeoPoint.latitude,
+                    lat2: startPoint.latitude,
+                    lon1: currentGeoPoint.longitude,
+                    lon2: startPoint.longitude
+                )
+                let proximityInMiles = Double(round(10 * (proximity * 0.000621371))/10)
+                println("PROXIMITY : \(proximity) meters")
+                println("PROXIMITY : \(proximityInMiles) miles")
+                cell.scavengerHuntProximity?.text = String(stringInterpolationSegment: proximityInMiles) + " miles"
+            }
+        }
         return cell
     }
     
